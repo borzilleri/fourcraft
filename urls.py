6fab4269ae248e1e7b121708e4b945f54606a184
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2011-08-25 11:07:37 -0700

    Add authentication views file
    - Begin implementation of authentication methods, probably going to
    	refactor this if I can find a good FS APIv2 library.

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from fourcraft.views.core import ( home )
+from fourcraft.views.auth import ( login )
 
 urlpatterns = patterns('',
     url('^fourcraft/$', home, name='home'),
-    #url('^fourcraft/login$', login, name='login'),
+    url('^fourcraft/login$', login, name='login'),
 )
