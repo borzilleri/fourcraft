72dc189c5e514b899cbfe58fe98da63956ae439d
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2011-09-24 09:46:34 -0700

    Removes foursquare prefix from urls
    - The foursquare prefix (if it exists) should go in the site's urls
    	file, not this apps.

--- urls.py
+++ urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 from fourcraft.views.core import ( home )
 
 urlpatterns = patterns('',
-    url('^fourcraft/$', home, name='home'),
-    #url('^fourcraft/login$', login, name='login'),
+    url('^$', home, name='home'),
+    #url('^login$', login, name='login'),
 )
